8dedcc21cc493bc4f090e256bde2430ff44142fa
khaledxab <khaledxab@gmail.com>
2022-09-08 18:04:47 +0100

    checks if a singly linked list is a palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,35 +9,66 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current;
-	int i, j, len, *arr;
+    listint_t *current, *next, *prev;
+    listint_t *slow, *fast;
+    int length, mid, i;
 
-	if (head == NULL || *head == NULL)
-		return (1);
-	current = *head;
-	len = 0;
-	while (current != NULL)
-	{
-		len++;
-		current = current->next;
-	}
-	arr = malloc(sizeof(int) * len);
-	if (arr == NULL)
-		return (0);
-	current = *head;
-	for (i = 0; i < len; i++)
-	{
-		arr[i] = current->n;
-		current = current->next;
-	}
-	for (i = 0, j = len - 1; i < len / 2; i++, j--)
-	{
-		if (arr[i] != arr[j])
-		{
-			free(arr);
-			return (0);
-		}
-	}
-	free(arr);
-	return (1);
+    /* empty list */
+    if (*head == NULL)
+        return (1);
+
+    /* single node list */
+    if ((*head)->next == NULL)
+        return (1);
+
+    /* double node list */
+    if ((*head)->next->next == NULL)
+    {
+        if ((*head)->n == (*head)->next->n)
+            return (1);
+        else
+            return (0);
+    }
+
+    /* find length of list */
+    current = *head;
+    length = 0;
+    while (current != NULL)
+    {
+        length++;
+        current = current->next;
+    }
+
+    /* find middle of list */
+    mid = length / 2;
+    current = *head;
+    i = 0;
+    while (i < mid)
+    {
+        i++;
+        current = current->next;
+    }
+
+    /* reverse second half of list */
+    prev = NULL;
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    /* compare first half and second half of list */
+    slow = *head;
+    fast = prev;
+    while (slow != NULL && fast != NULL)
+    {
+        if (slow->n != fast->n)
+            return (0);
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return (1);
 }
