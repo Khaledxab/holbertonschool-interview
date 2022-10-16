e1ca58877ea5f24a223a7ffb33d701287864f68c
Khaledxab <khaledxab@gmail.com>
2022-10-16 20:51:28 +0200

    slide line game fix

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,57 +1,100 @@
 #include "slide_line.h"
+/**
+ * reverse - reverse the array
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+void reverse(int *line, size_t size)
+{
+	size_t a, b;
+
+	a = 0;
+	b = size - 1;
+	while (a < b)
+	{
+		int temp = line[a];
+
+		line[a] = line[b];
+		line[b] = temp;
+		a++;
+		b--;
+	}
+}
 
 /**
- * slide_line - slides and merges an array of integers
- * @line: points to an array of integers
- * @size: number of elements in array
- * @direction: SLIDE_LEFT or SLIDE_RIGHT
- * Return: 1 upon success, or 0 upon failure
+ * zero - moves zeros
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+void zero(int *line, size_t size)
+{
+	size_t a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		if (line[a] == 0)
+		{
+			for (b = a + 1; b < size; b++)
+			{
+				if (line[b] != 0)
+				{
+					line[a] = line[b];
+					line[b] = 0;
+					break;
+				}
+			}
+		}
+	}
+}
+
+/**
+ * Nzeor - moves non zeros
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+void Nzeor(int *line, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (line[i] == line[i + 1])
+		{
+			line[i] = line[i] * 2;
+			line[i + 1] = 0;
+			i++;
+		}
+		i++;
+	}
+}
+/**
+ * slide_line - function that slides and merges an array of integers.
+ * @line: points to an array of integers containing
+ * @size: elements, that must be slided & merged to the
+ * @direction: represented by direction
+ * Return: 1 upon success, or 0 upon failure.
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	int i, j, k, l, m, n;
-
 	if (direction == SLIDE_LEFT)
 	{
-		for (i = 0; i < (int)size - 1; i++)
-		{
-			if (line[i] == line[i + 1] && line[i] != 0)
-			{
-				line[i] *= 2;
-				line[i + 1] = 0;
-				for (j = i + 1; j < (int)size - 1; j++)
-					line[j] = line[j + 1];
-				line[j] = 0;
-				i--;
-			}
-		}
+		zero(line, size);
+		Nzeor(line, size);
+		zero(line, size);
 	}
 	else if (direction == SLIDE_RIGHT)
 	{
-		for (k = (int)size - 1; k > 0; k--)
-		{
-			if (line[k] == line[k - 1] && line[k] != 0)
-			{
-				line[k] *= 2;
-				line[k - 1] = 0;
-				for (l = k - 1; l > 0; l--)
-					line[l] = line[l - 1];
-				line[l] = 0;
-				k++;
-			}
-		}
+		reverse(line, size);
+		zero(line, size);
+		Nzeor(line, size);
+		zero(line, size);
+		reverse(line, size);
 	}
 	else
 		return (0);
-	for (m = 0; m < (int)size; m++)
-	{
-		if (line[m] == 0)
-		{
-			for (n = m; n < (int)size - 1; n++)
-				line[n] = line[n + 1];
-			line[n] = 0;
-			m--;
-		}
-	}
 	return (1);
 }
