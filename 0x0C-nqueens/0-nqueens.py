6d0e669ac6d25708557eca2eb5d8df2c41ef69e0
Khaledxab <khaledxab@gmail.com>
2022-11-06 22:34:49 +0100

    N Queens

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""N queens puzzle"""
+import sys
+
+
+def nqueens(n: int):
+    """
+    tracking
+    """
+    m = [[0 for i in range(n)] for j in range(n)]
+    print(str(m))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+    nqueens(n)
