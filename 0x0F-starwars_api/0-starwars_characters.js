21f678034c8a8564020b12dea7cc2950c335f35e
khaledxab <khaledxab@gmail.com>
2022-11-24 23:11:39 +0100

    fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,24 +2,24 @@
 
 const request = require('request');
 
-const Id = process.argv[2];
-const url = `https://swapi.dev/api/films/${Id}/`;
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 request(url, async function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-            const name = await new Promise((resolve, reject) => {
-                request(character, (error, name, html) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(JSON.parse(html).name);
-                    }
-                });
-            });
-            console.log(name);
-        }
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
     }
+  }
 });
