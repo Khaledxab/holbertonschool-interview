c524e27088581318a9f8fcff45ee40d1b42cab6a
khaledxab <khaledxab@gmail.com>
2022-11-24 22:56:14 +0100

    FIX

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-        request(character, function (error, response, body) {
-            if (error) {
-            console.log(error);
-            } else {
-            console.log(JSON.parse(body).name);
-            }
+const request = require('request');
+
+const Id = process.argv[2];
+const url = `https://swapi.dev/api/films/${Id}/`;
+request(url, async function (error, nameponse, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const name = await new Promise((nameolve, reject) => {
+        request(character, (error, name, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            nameolve(JSON.parse(html).name);
+          }
         });
-        }
+      });
+      console.log(name);
     }
-    });
+  }
+});
