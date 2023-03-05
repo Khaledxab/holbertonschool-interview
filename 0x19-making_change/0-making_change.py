d3f72be77bed51592ff8884bdb0778dc4e5bf6c2
khaledxab <khaledxab@gmail.com>
2023-03-05 17:59:39 +0100

    Change comes from within

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Make change MM
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            change += 1
+    if total > 0:
+        return -1
+    return change
