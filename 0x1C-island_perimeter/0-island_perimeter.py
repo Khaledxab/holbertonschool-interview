389a7ae68cb31d80a5bbed84db303a5a0f576d11
khaledxab <khaledxab@gmail.com>
2023-03-19 21:48:35 +0100

    . island

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+def island_perimeter(grid):
+    # Check for empty grid
+    if not grid:
+        return 0
+    
+    # Get dimensions of the grid
+    rows, cols = len(grid), len(grid[0])
+    
+    # Initialize perimeter variable
+    perimeter = 0
+    
+    # Iterate over each cell in the grid
+    for row in range(rows):
+        for col in range(cols):
+            # Check if current cell is land
+            if grid[row][col] == 1:
+                # Check top, bottom, left, right cells
+                if row == 0 or grid[row-1][col] == 0:
+                    perimeter += 1
+                if row == rows-1 or grid[row+1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col-1] == 0:
+                    perimeter += 1
+                if col == cols-1 or grid[row][col+1] == 0:
+                    perimeter += 1
+    
+    # Return total perimeter
+    return perimeter
