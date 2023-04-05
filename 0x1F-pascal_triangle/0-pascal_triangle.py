9a2c6d3420564c85cf79420fd1c1fd55e76f06a0
khaledxab <khaledxab@gmail.com>
2023-04-05 23:43:10 +0200

    req fix 12

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -7,6 +7,7 @@ Pascal's Triangle
 def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
-        row = [1] + [triangle[i-1][j] + triangle[i-1][j-1] for j in range(1, i)] + [1]
+        row = [1] + [triangle[i - 1][j] + triangle[i - 1][j - 1]
+                     for j in range(1, i)] + [1]
         triangle.append(row)
     return triangle if n > 0 else []
